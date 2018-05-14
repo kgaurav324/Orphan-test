8d961f4fd4731108741ba7dd925e7f8b19e2efad
unknown <kgaurav@paytabs.com>
2018-05-14 15:08:23 +0530

    second commit

--- app/src/main/java/com/paytabs/orphanapp1/MapsActivity.java
+++ app/src/main/java/com/paytabs/orphanapp1/MapsActivity.java
@@ -2,6 +2,7 @@ package com.paytabs.orphanapp1;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,6 +14,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Button btPay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
